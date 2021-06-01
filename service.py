aaf16c75e157e5b2fefdd5aee2c4f74f4ec8a315
Daniel Lotsman <ddaannttee1155@gmail.com>
2021-06-01 15:27:00 +0300

    form JSON and multi OS check

--- service.py
+++ service.py
@@ -1,1 +1,10 @@
-import __hello__
+import os
+import json
+system = os.name
+
+if system == 'nt':
+	message = json.dumps({'msg': 'hello windows'})
+else:
+	message = json.dumps({'msg': 'hello linux and other'})
+
+print(message)
